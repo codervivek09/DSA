e2e8ca71b6b3935d04420622fdd3e02a6be2b937
Vivek Phad <vivekphad775@gmail.com>
2026-09-01 16:00:33 +0530

    Add Files via Upload

---
+++ Q00190_Reverse_Bits.java
@@ -0,0 +1,16 @@
+//Q.190 Reverse Bits
+
+class Q00190_Reverse_Bits {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for(int i=0; i<32; i++){
+            result = result << 1;
+            result = result + (n & 1);
+            n = n>>1;
+        }
+        return result;
+    }
+}
+
+
